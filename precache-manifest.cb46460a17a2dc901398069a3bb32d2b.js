c960012c1a442724090d72353ef53710f8e34a5e
Abhinav Mani <abhinav1946@gmail.com>
2020-04-17 12:55:41 +0530

    Updates

---
+++ precache-manifest.cb46460a17a2dc901398069a3bb32d2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8f1c6d375ab548bbf69276879ca091",
+    "url": "/New-York-Times/index.html"
+  },
+  {
+    "revision": "6ac23a642a91434b2761",
+    "url": "/New-York-Times/static/css/main.31db2713.chunk.css"
+  },
+  {
+    "revision": "1a181e79e711fbf34dd0",
+    "url": "/New-York-Times/static/js/2.3d5b36bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/New-York-Times/static/js/2.3d5b36bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac23a642a91434b2761",
+    "url": "/New-York-Times/static/js/main.643453fe.chunk.js"
+  },
+  {
+    "revision": "971628bc60adc7b570ca",
+    "url": "/New-York-Times/static/js/runtime-main.4f1b0678.js"
+  },
+  {
+    "revision": "588070a9838d5ef51eaba28f701eb07b",
+    "url": "/New-York-Times/static/media/button.588070a9.svg"
+  },
+  {
+    "revision": "c67174d0e0c88eb9beec35e6b14d8364",
+    "url": "/New-York-Times/static/media/noresults.c67174d0.svg"
+  }
+]);
